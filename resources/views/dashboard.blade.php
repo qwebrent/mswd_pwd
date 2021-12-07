6a55198bd83f5ab9fb27b2f41be4161de70cabef
Renwick Brent D. Corcega <qwebrent@gmail.com>
2021-12-07 15:17:29 +0800

    Added QR Scanner on Main Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,6 +9,23 @@
                 <div class="col-lg-5 col-md-6">
                     <h1 class="text-white">{{ __('Welcome to the Municipal Social Welfare Development Office of Calauan') }}</h1>
                 </div>
+
+                <div class="col">
+                        <video id="preview" class="p-1 shadow-sm mb-5 bg-white rounded" style="width: 350px;"></video>
+
+
+                    <h4 style="color:white">Scan QR Codes using your Camera</h4>
+                    {{-- <div class="btn-group btn-group-toggle mb-5" data-toggle="buttons">
+                      <label class="btn btn-primary active">
+                        <input type="radio" name="options" value="1" autocomplete="off" checked> Front Camera
+                      </label>
+                      <label class="btn btn-secondary">
+                        <input type="radio" name="options" value="2" autocomplete="off"> Back Camera
+                      </label>
+                    </div> --}}
+                </div>
+
+
             </div>
         </div>
     </div>
@@ -23,4 +40,41 @@
 
 
 
+@endsection
+
+@section("page_level_scripts")
+<script type="text/javascript">
+    var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
+    scanner.addListener('scan',function(content){
+        //alert(content);
+        //window.location.href=content;
+        window.open({{ 'content' }}, '_blank');
+    });
+    Instascan.Camera.getCameras().then(function (cameras){
+        if(cameras.length>0){
+            scanner.start(cameras[0]);
+            $('[name="options"]').on('change',function(){
+                if($(this).val()==1){
+                    if(cameras[0]!=""){
+                        scanner.start(cameras[0]);
+                    }else{
+                        alert('No Front camera found!');
+                    }
+                }else if($(this).val()==2){
+                    if(cameras[1]!=""){
+                        scanner.start(cameras[1]);
+                    }else{
+                        alert('No Back camera found!');
+                    }
+                }
+            });
+        }else{
+            console.error('No cameras found.');
+            alert('No cameras found.');
+        }
+    }).catch(function(e){
+        console.error(e);
+        alert(e);
+    });
+</script>
 @endsection
