c92fa418ae510869dde5553c4800297844baf1fc
Renwick Brent D. Corcega <qwebrent@gmail.com>
2022-04-07 18:50:00 +0800

    Added 'svg' File type (Not savable as image)

--- resources/views/layouts/qrCodePWD.blade.php
+++ resources/views/layouts/qrCodePWD.blade.php
@@ -1,11 +1,17 @@
 <div class="visible-print">
+    {!! QrCode::size(100)
+    ->format('svg')
+    ->size(130)
+    ->errorCorrection('H')
+    ->generate(Request::url()); !!}
+    <p>{{ $pwdinfo -> getFullNamePWD() }}</p>
+</div>
+
+{{-- <div class="visible-print">
     <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')
-    //->backgroundColor(106,109,228)
     ->style('round')
-    //->merge('https://www.clipartmax.com/png/middle/157-1575059_dswd-logo-philippines-department-of-social-welfare-and-development.png', 0.3, true)
     ->size(150)
     ->errorCorrection('H')
     ->generate(Request::url())) !!} ">
-    {{-- {!! QrCode::size(150)->generate(Request::url()); !!} --}}
     <p>{{ $pwdinfo -> getFullNamePWD() }}</p>
-</div>
+</div> --}}
